d89581c8c7414157f225a65ac4a63e534e4a3004
Reed Boyer <rpboyer15@gmail.com>
2025-04-25 13:26:46 -0700

    tmux config

--- .config/nvim/lua/reed/core/keymaps.lua
+++ .config/nvim/lua/reed/core/keymaps.lua
@@ -2,36 +2,36 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
--- escape sequence
+-- Escape sequence
 keymap.set("i", ".-", "<ESC>", { desc = "Exit insert mode with .-" })
 
--- clear search highlights
+-- Clear search highlights
 keymap.set("n", "<leader>vh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- move panes with winshift
+-- Move panes with winshift
 --
 
--- navigate panes with tmux-navigator
+-- Navigate panes with tmux-navigator
 -- C-haoe
 
--- maximize panes with vim-maximizer
+-- Maximize panes with vim-maximizer
 -- M-=
 
--- window management
-keymap.set("n", "<M-\\>", "<C-w>v", { desc = "Split window vertically" })    -- split window vertically
-keymap.set("n", "<M-->", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
-keymap.set("n", "<M-q>", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- Pane management
+keymap.set("n", "<M-\\>", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<M-->", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<M-=>", "<C-w>=", { desc = "Equalize splits" })
+keymap.set("n", "<M-w>", "<cmd>close<CR>", { desc = "Close current split" })
 
--- resize panes
-keymap.set("n", "<M-O>", ":resize +5<CR>", { desc = "Increase height" })
-keymap.set("n", "<M-A>", ":resize -5<CR>", { desc = "Decrease height" })
-keymap.set("n", "<M-E>", ":vertical resize +5<CR>", { desc = "Increase width" })
-keymap.set("n", "<M-H>", ":vertical resize -5<CR>", { desc = "Decrease width" })
+-- Resize panes
+keymap.set("n", "<M-o>", ":resize +5<CR>", { desc = "Increase height" })
+keymap.set("n", "<M-a>", ":resize -5<CR>", { desc = "Decrease height" })
+keymap.set("n", "<M-e>", ":vertical resize +5<CR>", { desc = "Increase width" })
+keymap.set("n", "<M-h>", ":vertical resize -5<CR>", { desc = "Decrease width" })
 
 -- Tab navigation
-keymap.set("n", "<M-d>", "<cmd>tabnew<CR>", { desc = "New tab" })                               -- open new tab
-keymap.set("n", "<M-q>", "<cmd>tabclose<CR>", { desc = "Close current tab" })                   -- close current tab
-keymap.set("n", "<M-w>", "<cmd>tabn<CR>", { desc = "Go to next tab" })                          --  go to next tab
-keymap.set("n", "<M-l>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<M-t>", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<M-q>", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<M-s>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<M-r>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
