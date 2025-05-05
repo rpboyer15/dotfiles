f23c8ecd121811630f47fa68e3f82d5304e9b782
Reed Boyer <rpboyer15@gmail.com>
2025-05-05 12:43:16 -0700

    update

--- .config/nvim/lua/reed/core/keymaps.lua
+++ .config/nvim/lua/reed/core/keymaps.lua
@@ -21,7 +21,7 @@ keymap.set("n", "<leader>vh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<M-\\>", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<M-->", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<M-=>", "<C-w>=", { desc = "Equalize splits" })
-keymap.set("n", "<M-w>", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<M-q>", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- Resize panes
 keymap.set("n", "<M-o>", ":resize +5<CR>", { desc = "Increase height" })
@@ -30,8 +30,7 @@ keymap.set("n", "<M-e>", ":vertical resize +5<CR>", { desc = "Increase width" })
 keymap.set("n", "<M-h>", ":vertical resize -5<CR>", { desc = "Decrease width" })
 
 -- Tab navigation
-keymap.set("n", "<M-t>", "<cmd>tabnew<CR>", { desc = "New tab" })
--- keymap.set("n", "<M-q>", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<M-s>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<M-r>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<M-t>", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<M-s>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
